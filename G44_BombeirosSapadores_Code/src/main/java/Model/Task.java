2aae0f27af334c96f3aeaddcdbad0f3bc1c5d071
Jorge Silva <1191458@isep.ipp.pt>
2020-06-01 19:13:32 +0100

    [Implementation] Created the class Task

---
+++ G44_BombeirosSapadores_Code/src/main/java/Model/Task.java
@@ -0,0 +1,37 @@
+
+package Model;
+
+import Utils.Time;
+
+/**
+ *
+ * @author jorge
+ */
+public class Task {
+    
+    /**
+     * The id of the Task
+     */
+    String idTask;
+    
+    /**
+     * The description of the Task
+     */
+    String description;
+    
+    /**
+     * 
+     */
+    Time time;
+    
+    double costHour;
+    
+    String TaskCategory;
+    
+    boolean isPayed;
+    
+    boolean isFinished;
+    
+    
+    
+}
