326081749ca7a94bfc8319a053c57933f249270e
alexrosa336 <1190336@isep.ipp.pt>
2020-06-02 12:46:24 +0100

    [Implementation] - start doing the UC4 code

--- G44_BombeirosSapadores_Code/src/main/java/Model/RegisterOrganization.java
+++ G44_BombeirosSapadores_Code/src/main/java/Model/RegisterOrganization.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-    package Controller;
+    package Model;
 
+import Controller.ApplicationPOT;
 import Model.Address;
 import Model.Collaborator;
 import Model.Manager;
---
+++ G44_BombeirosSapadores_Code/src/main/java/UI/CreateTransactionUI.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+import Controller.CreateTransactionController;
+
+/**
+ *
+ * @author OMEN X
+ */
+public class CreateTransactionUI {
+    private CreateTransactionController ct_controller;
+    
+    public CreateTransactionUI (){
+        ct_controller = new CreateTransactionController();
+    }
+    
+    
+}
