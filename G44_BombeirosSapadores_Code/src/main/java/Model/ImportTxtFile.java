71bc179820da5948834403ad98d6c8dfb3fdc7f0
Bruno Pereira <1190452@isep.ipp.pt>
2020-06-02 11:11:13 +0100

    [Implementation] - new constructor task

--- G44_BombeirosSapadores_Code/src/main/java/Model/ImportTxtFile.java
+++ G44_BombeirosSapadores_Code/src/main/java/Model/ImportTxtFile.java
@@ -6,10 +6,45 @@
 
 package Model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 /**
  *
  * @author tiagopereira
  */
-public class ImportTxtFile {
+public class ImportTxtFile implements ImportFile {
+    private HistoricalTransaction ht;
+    @Override
+    public void importFile(List<Transaction> transactions, String fileName) {
+        
+        try {
+            Scanner sc = new Scanner(new File(fileName), "utf-8");
+            sc.useDelimiter(";|\n");
+            ht = new HistoricalTransaction();
+            try {
+                while (sc.hasNext()) {
+                   String transID = sc.next().trim();
+                   String taskID = sc.next().trim();
+                   Time time = (Time) sc.next
+                   
+                }
+                
+            } catch (NoSuchElementException e) {
+                
+                System.out.println("Error reading HistoricalTransaction file!");
+                
+            }
+            
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("O ficheiro que está a tentar carregar nao existe.");
+            
+        }
+    
+    }
 
 }
