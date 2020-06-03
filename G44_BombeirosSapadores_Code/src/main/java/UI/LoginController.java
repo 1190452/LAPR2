11eb8fb3180f4f46d3194f5aad317e2f46e05764
Jorge Silva <1191458@isep.ipp.pt>
2020-06-03 20:44:28 +0100

    [Implementation] Added some UI and fxml classes

--- G44_BombeirosSapadores_Code/src/main/java/UI/LoginController.java
+++ G44_BombeirosSapadores_Code/src/main/java/UI/LoginController.java
@@ -1,13 +1,17 @@
 
 package UI;
 
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -17,7 +21,7 @@ import javafx.scene.control.TextField;
 public class LoginController implements Initializable {
 
     @FXML
-    private TextField txtFieldUsername;
+    private JFXTextField txtFieldUsername;
     @FXML
     private PasswordField txtFieldPassword;
     @FXML
@@ -32,5 +36,17 @@ public class LoginController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void login(ActionEvent event) {
+    }
+
+    @FXML
+    private void exit(ActionEvent event) {
+    }
+
+    @FXML
+    private void exit(MouseEvent event) {
+    }
     
 }
