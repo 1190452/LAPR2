7914b8736853fc99f7de2d8bb9ffd72c27e72a90
oliveirarafa101 <1191611@isep.ipp.pt>
2020-06-04 19:24:35 +0100

    [UC10][UPDATE]

--- G44_BombeirosSapadores_Code/src/main/java/UI/CreateFreelancerUI.java
+++ G44_BombeirosSapadores_Code/src/main/java/UI/CreateFreelancerUI.java
@@ -51,8 +51,7 @@ public class CreateFreelancerUI implements Initializable {
     
     
     
-    
-    @Override
+  
     public void initialize(URL location, ResourceBundle resources) {
         ObservableList<String> list = FXCollections.observableArrayList("Junior", "Senior");
         cbLvl.setItems(list);
