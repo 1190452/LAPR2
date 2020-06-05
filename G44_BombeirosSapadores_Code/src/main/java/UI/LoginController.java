1b48c0e99fbcdb831ce837fdae2750cd8527ed67
Jorge Silva <1191458@isep.ipp.pt>
2020-06-05 11:54:06 +0100

    [Implementation] UI

--- G44_BombeirosSapadores_Code/src/main/java/UI/LoginController.java
+++ G44_BombeirosSapadores_Code/src/main/java/UI/LoginController.java
@@ -1,17 +1,24 @@
 
 package UI;
 
+import Model.Constants;
+import autorizacao.FacadeAuthorization;
 import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
@@ -19,7 +26,11 @@ import javafx.stage.Stage;
  * @author Jorge
  */
 public class LoginController implements Initializable {
-
+    
+    private static final String MANAGER_ROLE = "MANAGER";
+    private static final String ADMINISTRATOR_ROLE = "ADMINISTRATOR";
+    private static final String COLLABORATOR_ROLE = "COLLABORATOR";
+    
     @FXML
     private JFXTextField txtFieldUsername;
     @FXML
@@ -40,6 +51,30 @@ public class LoginController implements Initializable {
 
     @FXML
     private void login(ActionEvent event) {
+        try {
+            FacadeAuthorization fa = new FacadeAuthorization();
+            fa.doLogin(txtFieldUsername.getText(), txtFieldPassword.getText());
+            if (FacadeAuthorization.getActualSession().isLoggedInWithPart(Constants.ROLE_ADMINISTRATIVE)) {
+                
+            }
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/MenuManager.fxml"));
+            
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Login Window");
+            stage.setScene(new Scene(root1));
+            stage.setResizable(false);
+            stage.initStyle(StageStyle.TRANSPARENT);
+            stage.show();
+            
+            // Hide this current window
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+        
+        
+        
     }
 
 
