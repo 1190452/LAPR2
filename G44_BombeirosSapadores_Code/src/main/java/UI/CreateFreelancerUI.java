69d9d285f492ef7e9408b0efc9df9ad39b290813
Bruno Pereira <1190452@isep.ipp.pt>
2020-06-05 12:16:12 +0100

    [Implementation] - Doing create freelancer interface

--- G44_BombeirosSapadores_Code/src/main/java/UI/CreateFreelancerUI.java
+++ G44_BombeirosSapadores_Code/src/main/java/UI/CreateFreelancerUI.java
@@ -10,6 +10,7 @@ import Model.Freelancer;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -17,6 +18,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -25,6 +27,7 @@ import javafx.stage.Stage;
  * @author User
  */
 public class CreateFreelancerUI implements Initializable {
+
     private CreateFreelancerController cf;
     @FXML
     private Button cancelBtn;
@@ -76,29 +79,34 @@ public class CreateFreelancerUI implements Initializable {
 
     @FXML
     private void confirmBtn(ActionEvent event) {
-       try {
-        String name = nameTxt.getSelectedText();
-        String email = emailTxt.getSelectedText();
-        String nif = nifTxt.getSelectedText();
-        String iban = ibanTxt.getSelectedText();
-        String country = countryTxt.getSelectedText();
-        String street = streetTxt.getSelectedText();
-        String door = doorTxt.getSelectedText();
-        String locality = localityTxt.getSelectedText();
-        String exp = experienceCB.getSelectionModel().getSelectedItem();
-        Freelancer freel = cf.newFreelancer(name, exp, email, nif, iban, country, street, door, locality);
-        
-        boolean added =cf.saveFreelancer();
-        AlertUI.criarAlerta(Alert.AlertType.INFORMATION, MainApp.APPLICATION_TITLE, "Add new Freelancer.",
-                    added ? "Freelancer added with success."
-                    : "It was not possible to add Freelancer").show();
-       } catch (NumberFormatException nfe) {
+        try {
+            String name = nameTxt.getSelectedText();
+            String email = emailTxt.getSelectedText();
+            String nif = nifTxt.getSelectedText();
+            String iban = ibanTxt.getSelectedText();
+            String country = countryTxt.getSelectedText();
+            String street = streetTxt.getSelectedText();
+            String door = doorTxt.getSelectedText();
+            String locality = localityTxt.getSelectedText();
+            String exp = experienceCB.getSelectionModel().getSelectedItem();
+            cf.newFreelancer(name, exp, email, nif, iban, country, street, door, locality);
+            if (alert1(event) == ButtonType.YES) {
+                boolean added = cf.saveFreelancer();
+                AlertUI.criarAlerta(Alert.AlertType.INFORMATION, MainApp.APPLICATION_TITLE, "Add new Freelancer.",
+                        added ? "Freelancer added with success."
+                                : "It was not possible to add Freelancer").show();
+                encerrarCreateFreelancerUI(event);
+            } else if(alert1(event) == ButtonType.NO){
+                encerrarCreateFreelancerUI(event);
+            }
+
+        } catch (NumberFormatException nfe) {
             AlertUI.criarAlerta(Alert.AlertType.ERROR, MainApp.APPLICATION_TITLE, "Error in data.",
                     "Introduza correct data").show();
         } catch (IllegalArgumentException iae) {
             AlertUI.criarAlerta(Alert.AlertType.ERROR, MainApp.APPLICATION_TITLE, "Error in data.",
                     iae.getMessage()).show();
-       }
+        }
     }
 
     @FXML
@@ -111,6 +119,14 @@ public class CreateFreelancerUI implements Initializable {
     private void close(MouseEvent event) {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.close();
+    }
+
+    @FXML
+    private ButtonType alert1(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Freelancer");
+        Optional<ButtonType> result = alert.showAndWait();
+        return result.get();
     }
 
 }
