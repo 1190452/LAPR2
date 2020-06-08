05cdfcc72933582772142845eb92ee95eceb0384
alexrosa336 <1190336@isep.ipp.pt>
2020-06-08 23:06:16 +0100

    [Implementation] - finishing JavaDoc in all packages except UI

--- G44_BombeirosSapadores_Code/src/main/java/Authorization/model/RegisterUser.java
+++ G44_BombeirosSapadores_Code/src/main/java/Authorization/model/RegisterUser.java
@@ -14,29 +14,58 @@ import java.util.List;
  *
  * @author paulomaio
  */
-public class RegisterUser
-        implements Serializable {
+public class RegisterUser implements Serializable {
 
+    /**
+     * list of users
+     */
     private List<User> lstUsers;
-    
+
+    /**
+     * platform that contains the users
+     */
     private Platform platform;
 
+    /**
+     * constructor that initializes the list of users as an ArrayList
+     */
     public RegisterUser() {
         this.lstUsers = new ArrayList<>();
     }
 
+    /**
+     * returns the list of users
+     * @return 
+     */
     public List<User> getUserList() {
         return lstUsers;
     }
 
+    /**
+     * modifies the list of users
+     * @param usersList 
+     */
     public void setUsersList(ArrayList<User> usersList) {
         lstUsers = usersList;
     }
 
+    /**
+     * method that creates a new user
+     * @param name
+     * @param strEmail
+     * @param strPassword
+     * @param strRole
+     * @return 
+     */
     public User newUser(String name, String strEmail, String strPassword, String strRole) {
         return new User(name, strEmail, strPassword, strRole);
     }
 
+    /**
+     * adds a user to the list of users
+     * @param utlz
+     * @return 
+     */
     public boolean addUser(User utlz) {
 //        if (hasUser(utlz)) {
 //            throw new IllegalArgumentException("User Already Exists");
@@ -44,13 +73,11 @@ public class RegisterUser
         return this.lstUsers.add(utlz);
     }
 
-    public void removeUser(User utlz) {
-//        if (!hasUser(utlz)) {
-//            throw new IllegalArgumentException("User does not exist");
-//        }
-        platform.getrUser().remove(utlz);
-    }
-
+    /**
+     * method that finds a user in the list of users by his email and returns him
+     * @param strEmail
+     * @return 
+     */
     public User findUser(String strEmail) {
         for (User utlz : lstUsers) {
             if (utlz.hasEmail(strEmail)) {
@@ -60,6 +87,11 @@ public class RegisterUser
         return null;
     }
 
+    /**
+     * method that checks if a platform contains a certain user receiving an email by parameter
+     * @param strEmail
+     * @return 
+     */
     public boolean hasUser(String strEmail) {
         User utlz = findUser(strEmail);
         if (utlz != null) {
@@ -68,10 +100,20 @@ public class RegisterUser
         return false;
     }
 
+    /**
+     * method that checks if a platform contains a certain user receiving an user by parameter
+     * @param utlz
+     * @return 
+     */
     public boolean hasUser(User utlz) {
         return platform.getrUser().getUserList().contains(utlz);
     }
 
+    /**
+     * removes a user from the list of users
+     * @param utlz
+     * @return 
+     */
     public boolean remove(User utlz) {
         if (lstUsers.contains(utlz)) {
             return lstUsers.remove(utlz);
--- G44_BombeirosSapadores_Code/src/main/java/Authorization/model/UserSession.java
+++ G44_BombeirosSapadores_Code/src/main/java/Authorization/model/UserSession.java
@@ -5,8 +5,16 @@ import java.io.Serializable;
 
 public class UserSession implements Serializable {
 
+    /**
+     * user that is logged in
+     */
     private User user = null;
 
+    /**
+     * constructor than builds an instance of UserSession
+     *
+     * @param user
+     */
     public UserSession(User user) {
         if (user == null) {
             throw new IllegalArgumentException("Argumento não pode ser nulo.");
@@ -14,14 +22,28 @@ public class UserSession implements Serializable {
         this.user = user;
     }
 
+    /**
+     * method that logs out the user
+     */
     public void doLogout() {
         this.user = null;
     }
 
+    /**
+     * checks if the user is logged in
+     *
+     * @return
+     */
     public boolean isLoggedIn() {
         return this.user != null;
     }
 
+    /**
+     * checks if user is logged in with a role
+     *
+     * @param strRole
+     * @return
+     */
     public boolean isLoggedInWithPart(String strRole) {
         if (isLoggedIn()) {
             return this.user.getRole().equalsIgnoreCase(strRole);
@@ -29,6 +51,11 @@ public class UserSession implements Serializable {
         return false;
     }
 
+    /**
+     * returns the email of the logged in user
+     *
+     * @return
+     */
     public String getUserEmail() {
         if (isLoggedIn()) {
             return this.user.getEmail();
@@ -36,6 +63,11 @@ public class UserSession implements Serializable {
         return null;
     }
 
+    /**
+     * returns the user name of the logged user
+     *
+     * @return
+     */
     public String getUserName() {
         if (isLoggedIn()) {
             return this.user.getName();
@@ -43,10 +75,20 @@ public class UserSession implements Serializable {
         return null;
     }
 
+    /**
+     * returns the logged user
+     *
+     * @return
+     */
     public User getUserBySession() {
         return this.user;
     }
 
+    /**
+     * writing method of the class UserSession
+     *
+     * @return
+     */
     @Override
     public String toString() {
         return "UserSession{" + "user=" + user.toString() + '}';
