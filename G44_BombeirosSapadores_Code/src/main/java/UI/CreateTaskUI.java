8adc4bf0bc99cf348f6a2d7e6b74a3a21190c6fb
1190990 <1190990@isep.ipp.pt>
2020-06-09 02:34:09 +0100

    [Implementation] - Validations

--- G44_BombeirosSapadores_Code/src/main/java/UI/CreateTaskUI.java
+++ G44_BombeirosSapadores_Code/src/main/java/UI/CreateTaskUI.java
@@ -7,6 +7,7 @@ package UI;
 
 import Controller.CreateTaskController;
 import Model.Task;
+import Utils.Validations;
 import com.jfoenix.controls.JFXTextField;
 import java.io.IOException;
 import java.util.Optional;
@@ -62,48 +63,51 @@ public class CreateTaskUI {
     @FXML
     private void confirm(ActionEvent event) throws InterruptedException, IOException {
 
-        try {
+        if (idTxt.getText().isEmpty() || descriptionTxt.getText().isEmpty() || timeTxt.getText().isEmpty() || costTxt.getText().isEmpty() || categoryTxt.getText().isEmpty()) {
+            Alert alert2 = AlertUI.createAlert(Alert.AlertType.ERROR, MainApp.APPLICATION_TITLE, "Task Creation", "Please introduce the necessary data to create a Task.");
+            alert2.show();
+        } else {
+            if (Validations.isNumeric(costTxt.getText()) && Validations.isNumeric(costTxt.getText())) {
+                if (Validations.isNameValid(categoryTxt.getText())) {
+                    ctask_controller = new CreateTaskController();
+                    Task ts = ctask_controller.newTask(idTxt.getText(), descriptionTxt.getText(),
+                            Integer.parseInt(timeTxt.getText()), Double.parseDouble(costTxt.getText()), categoryTxt.getText());
+                    Alert alert1 = AlertUI.createAlert(Alert.AlertType.INFORMATION, MainApp.APPLICATION_TITLE, "Task Creation", ts.toString());
 
-            if (idTxt.getText().isEmpty() || descriptionTxt.getText().isEmpty() || timeTxt.getText().isEmpty() || costTxt.getText().isEmpty() || categoryTxt.getText().isEmpty()) {
-                Alert alert2 = AlertUI.createAlert(Alert.AlertType.ERROR, MainApp.APPLICATION_TITLE, "Task Creation", "Please introduce the necessary data to create a Task.");
-                alert2.show();
+                    if (alert1.showAndWait().get() == ButtonType.OK) {
+                        alert1.close();
+                    }
+
+                    Alert alert6 = AlertUI.createAlert(Alert.AlertType.CONFIRMATION, MainApp.APPLICATION_TITLE, "Task Creation", "Do you confirm this task?");
+                    boolean verif = false;
+
+                    Optional<ButtonType> option = alert6.showAndWait();
+                    if (option.get() == ButtonType.OK) {
+                        verif = ctask_controller.registersTask();
+                    } else {
+                        alert1.close();
+                    }
+
+                    if (verif) {
+                        AlertUI.createAlert(Alert.AlertType.INFORMATION, MainApp.APPLICATION_TITLE, "Adding new Task",
+                                verif ? "New Task added with success"
+                                        : "It was not possible to add the Task").show();
+
+                        endTask(event);
+
+                    }
+                }else{
+                     Alert alert = AlertUI.createAlert(Alert.AlertType.ERROR, MainApp.APPLICATION_TITLE, "Task Error", "Please introduce a valid Category.");
+                    alert.show();
+                }
+
             } else {
-                ctask_controller = new CreateTaskController();
-                Task ts = ctask_controller.newTask(idTxt.getText(), descriptionTxt.getText(),
-                        Integer.parseInt(timeTxt.getText()), Double.parseDouble(costTxt.getText()), categoryTxt.getText());
-                Alert alert1 = AlertUI.createAlert(Alert.AlertType.INFORMATION, MainApp.APPLICATION_TITLE, "Task Creation", ts.toString());
-
-                if (alert1.showAndWait().get() == ButtonType.OK) {
-                    alert1.close();
-                }
-
-                Alert alert6 = AlertUI.createAlert(Alert.AlertType.CONFIRMATION, MainApp.APPLICATION_TITLE, "Task Creation", "Do you confirm this task?");
-                boolean verif = false;
-
-                Optional<ButtonType> option = alert6.showAndWait();
-                if (option.get() == ButtonType.OK) {
-                    verif = ctask_controller.registersTask();
-                } else {
-                    alert1.close();
-                }
-
-                if (verif) {
-                    AlertUI.createAlert(Alert.AlertType.INFORMATION, MainApp.APPLICATION_TITLE, "Adding new Task",
-                            verif ? "New Task added with success"
-                                    : "It was not possible to add the Task").show();
-
-                    endTask(event);
-
-                }
-
+                Alert alert = AlertUI.createAlert(Alert.AlertType.ERROR, MainApp.APPLICATION_TITLE, "Task Error", "Please introduce valid numbers for the cost and hour.");
+                alert.show();
             }
-        } catch (NumberFormatException nfe) {
-            AlertUI.createAlert(Alert.AlertType.ERROR, MainApp.APPLICATION_TITLE, "Error in data.",
-                    nfe.getMessage()).show();
-        } catch (IllegalArgumentException iae) {
-            AlertUI.createAlert(Alert.AlertType.ERROR, MainApp.APPLICATION_TITLE, "Error in data.",
-                    iae.getMessage()).show();
+
         }
+
     }
 
     @FXML
